d174df69a2249692aefd4c597e3ce4f893047687
brettm2277 <brettm2277@gmail.com>
2017-03-28 18:14:29 -0400

    Loading message while waiting for search.

--- MEAN/public/js/controllers/SearchCtrl.js
+++ MEAN/public/js/controllers/SearchCtrl.js
@@ -1,15 +1,23 @@
 angular.module('SearchCtrl', []).controller('SearchController', ['$scope', '$http', function ($scope, $http) {
 
+    $scope.searchQuery = "";
+    $scope.inputLocation = "";
+    $scope.errorMessage = "";
     $scope.isError = false;
+    $scope.isLoading = false;
+
     $scope.SearchFunction = function () {
         $scope.searchResults = {};
         var query = $scope.searchQuery;
         var location = $scope.inputLocation;
-        $scope.errorMessage = "";
-        if (query === "" && location === "") {
+        if ((query === "" || query === null) && (location === "" || location === null)) {
             $scope.isError = true;
             $scope.errorMessage = "Please enter either a location or event."
+            $scope.isLoading = false;
         } else {
+            $scope.isLoading = true;
+            $scope.errorMessage = "";
+            console.log("query: " + query);
             $scope.isError = false;
             $http({
                 method: 'GET',
@@ -19,9 +27,11 @@ angular.module('SearchCtrl', []).controller('SearchController', ['$scope', '$htt
                     'location': location
                 }
             }).then(function successCallback(success) {
+                $scope.isLoading = false;
                 $scope.searchResults = success.data;
                 console.log(success);
             }, function errorCallback(error) {
+                $scope.isLoading = false;
                 console.log(error);
             });
         }
