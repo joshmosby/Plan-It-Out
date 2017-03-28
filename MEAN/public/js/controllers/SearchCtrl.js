a013e8b4f3c67be0a8f3cbb9df502ae731f30b5f
brettm2277 <brettm2277@gmail.com>
2017-03-28 17:56:36 -0400

    Added error message when query and location are both blank.

--- MEAN/public/js/controllers/SearchCtrl.js
+++ MEAN/public/js/controllers/SearchCtrl.js
@@ -1,21 +1,29 @@
 angular.module('SearchCtrl', []).controller('SearchController', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.searchResults = {};
+    $scope.isError = false;
     $scope.SearchFunction = function () {
+        $scope.searchResults = {};
         var query = $scope.searchQuery;
         var location = $scope.inputLocation;
-        $http({
-            method: 'GET',
-            url: '/api/search',
-            headers: {
-                'query': query,
-                'location': location
-            }
-        }).then(function successCallback(success) {
-            $scope.searchResults = success.data;
-            console.log(success);
-        }, function errorCallback(error) {
-            console.log(error);
-        });
+        $scope.errorMessage = "";
+        if (query === "" && location === "") {
+            $scope.isError = true;
+            $scope.errorMessage = "Please enter either a location or event."
+        } else {
+            $scope.isError = false;
+            $http({
+                method: 'GET',
+                url: '/api/search',
+                headers: {
+                    'query': query,
+                    'location': location
+                }
+            }).then(function successCallback(success) {
+                $scope.searchResults = success.data;
+                console.log(success);
+            }, function errorCallback(error) {
+                console.log(error);
+            });
+        }
     };
 }]);
